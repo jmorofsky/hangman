47e6ced32176826aa86d89132b0f89a7f30379ea
Jason Morofsky <jmorofsky@gmail.com>
2023-01-25 18:37:03 -0500

    initial commit; title screen styling

---
+++ README.md
@@ -0,0 +1,4 @@
+hangman game
+utilizing the random word api from https://random-word-api.herokuapp.com/
+
+coded by Jason Morofsky
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './css/App.css'
+import TitleScreen from './components/TitleScreen'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <TitleScreen />
+      </div>
+    )
+  }
+}
+
+export default App
